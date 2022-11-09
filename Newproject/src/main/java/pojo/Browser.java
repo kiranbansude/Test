3711e69ad6388f94d8b571f585818ceced66cf06
Lenovo <Lenovo@DESKTOP-AKTE995>
2022-11-09 23:38:02 +0530

    added class browser.java

---
+++ Newproject/src/main/java/pojo/Browser.java
@@ -0,0 +1,21 @@
+package pojo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Browser {
+	
+
+	public static WebDriver launchChrome () {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.saucedemo.com");
+		driver.manage().window().maximize();
+		return driver;
+		
+	}
+
+
+}
